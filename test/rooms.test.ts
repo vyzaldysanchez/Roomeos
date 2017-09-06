0d529d6033e5beaa9b5611a3bacd7fcaa3f17325
Diego Michel <diego.ivan.perez.michel@gmail.com>
2017-09-05 22:05:46 -0400

    adds rooms controller test

---
+++ test/rooms.test.ts
@@ -0,0 +1,14 @@
+import {} from "jest";
+import * as supertest from "supertest";
+
+const app = require("../src/app");
+const request = supertest(app);
+
+describe("GET /rooms/discover", () => {
+
+  it("should return 302 Found when unauthenticated", () => {
+    return request.get("/rooms/discover")
+      .expect(302);
+  });
+
+});
