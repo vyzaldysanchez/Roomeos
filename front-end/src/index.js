e97226d24dff52fe500bea0045b8dcc54512e256
Diego Michel <diego.ivan.perez.michel@gmail.com>
2017-09-06 20:53:36 -0400

    adds setup for loading react apps

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<h1>Klk</h1>, document.getElementById('root'));
+loadComponent('discoverRooms');
+
+function loadComponent(componentName) {
+  const componentEl = document.getElementById(componentName);
+
+  if (componentEl) {
+    ReactDOM.render(<App/>, componentEl);
+  }
+}
