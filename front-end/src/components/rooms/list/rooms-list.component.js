cd0600417b8c1e236e3905f1a2206770b034e51d
Diego Michel <diego.ivan.perez.michel@gmail.com>
2017-10-11 23:59:57 -0400

    adds link to rooms

--- front-end/src/components/rooms/list/rooms-list.component.js
+++ front-end/src/components/rooms/list/rooms-list.component.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { RoomCard } from "../card/room-card.component";
 import "./rooms-list.component.scss";
 import { User } from "../../../models";
+import type { ChatRoom } from "../../../models/chat-room";
 
 export class RoomsList extends Component {
 
@@ -20,10 +21,12 @@ export class RoomsList extends Component {
           <h3>{this.props.title}</h3>
         </div>
         <div className="row">
-          {this.props.rooms.map((room, index) => {
+          {this.props.rooms.map((room: ChatRoom, index) => {
             return room ? (
               <div key={index} className="rooms-list-col col-md-3">
-                <RoomCard user={this.props.user} room={room}/>
+                <a href={`/rooms/${room._id}`}>
+                  <RoomCard user={this.props.user} room={room}/>
+                </a>
               </div>
             ) : null
           })}
