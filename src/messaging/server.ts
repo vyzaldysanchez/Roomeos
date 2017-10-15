807bb8b7839d7af370a04976483555e277496468
Diego Michel <diego.ivan.perez.michel@gmail.com>
2017-10-15 13:24:57 -0400

    extracts messaging events into separate module

--- src/messaging/server.ts
+++ src/messaging/server.ts
@@ -1,35 +1,22 @@
 import * as socketIO from "socket.io";
 import { Server } from "http";
-import { RoomMessageModel } from "../models/RoomMessage";
+import handleMessageEvents from "./messaging-events";
 
 export class MessagingServer {
-  private io: any;
+  private io: SocketIO.Server;
 
   constructor(private httpServer: Server) {
-    this.sockets();
+    this.setUpSocket();
   }
 
-  private sockets(): void {
+  private setUpSocket(): void {
     this.io = socketIO();
   }
 
   public listen(): void {
     this.io.listen(this.httpServer);
     console.log("Messaging server listening");
-
-    this.io.on("connect", (socket: any) => {
-
-      console.log("client connected");
-
-      socket.on("message", (m: RoomMessageModel) => {
-        console.log("[server](message): %s", JSON.stringify(m));
-        this.io.emit("message", m);
-      });
-
-      socket.on("disconnect", () => {
-        console.log("Client disconnected");
-      });
-    });
-
+    handleMessageEvents(this.io);
   }
+
 }
