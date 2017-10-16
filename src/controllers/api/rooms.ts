f255c139342b1741bb2046da81c906c909c1bc03
Diego Michel <diego.ivan.perez.michel@gmail.com>
2017-10-15 20:31:36 -0400

    adds getChatRoom api method

--- src/controllers/api/rooms.ts
+++ src/controllers/api/rooms.ts
@@ -62,3 +62,48 @@ export let getMyRooms = (req: Request, res: Response) => {
   });
 
 };
+
+/**
+ * GET /rooms/:id
+ */
+export let getChatRoom = async (req: Request, res: Response) => {
+
+  const responseRender = new ApiResponseRender(res);
+  const roomId = req.params.id;
+  const roomProjection = {
+    _id: 1,
+    name: 1,
+    description: 1,
+    visibility: 1,
+    maxChatters: 1,
+    registerChatters: 1,
+    messages: 1,
+  };
+  const messageProjection = {
+    _id: 1,
+    content: 1,
+    messageLevel: 1,
+    mentions: 1,
+    createdAt: 1,
+    sentBy: 1,
+  };
+
+  try {
+    const room = <RoomModel>await Room.findById(roomId, roomProjection).populate({
+      path: "messages",
+      select: messageProjection,
+      options: {
+        limit: 20,
+        sort: {createdAt: -1}
+      }
+    });
+
+    responseRender.render(
+      room.toObject()
+    );
+  }
+  catch (error) {
+    responseRender.renderValidationError(error);
+  }
+
+};
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,6 +6,7 @@ import * as AccountApiController from "../controllers/api/account";
 const router = Router();
 
 router.post("/rooms", passportConfig.isApiAuthenticated, RoomsApiController.postCreateRoom);
+router.get("/rooms/:id", passportConfig.isApiAuthenticated, RoomsApiController.getChatRoom);
 router.get("/users/:userId/rooms", passportConfig.isApiAuthenticated, RoomsApiController.getMyRooms);
 router.get("/account", passportConfig.isApiAuthenticated, AccountApiController.getAccountInfo);
 
